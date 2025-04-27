5e4bdd5efdb56af58b284397d84240deba9c60ca
s4mp41xao <samuelpaixao@proton.me>
2025-04-26 23:45:05 -0300

    feat: create signup page

---
+++ src/app/pages/signup/signup.component.spec.ts
@@ -0,0 +1,22 @@
+// import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+// import { SignUpComponent } from './signup.component';
+
+// describe('LoginComponent', () => {
+//   let component: SignUpComponent;
+//   let fixture: ComponentFixture<SignUpComponent>;
+
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [LoginComponent],
+//     }).compileComponents();
+
+//     fixture = TestBed.createComponent(LoginComponent);
+//     component = fixture.componentInstance;
+//     fixture.detectChanges();
+//   });
+
+//   it('should create', () => {
+//     expect(component).toBeTruthy();
+//   });
+// });
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -11,33 +11,40 @@ import { Router } from '@angular/router';
 import { LoginService } from '../../services/login.service';
 import { ToastrService } from 'ngx-toastr';
 
-interface LoginForm {
+interface SignupForm {
+  name: FormControl;
   email: FormControl;
   password: FormControl;
+  passwordConfirm: FormControl;
 }
 
 @Component({
-  selector: 'app-login',
+  selector: 'app-signup',
   imports: [
     DefaultLoginLayoutComponent,
     ReactiveFormsModule,
     DefaultInputButtonComponent,
   ],
   providers: [LoginService],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css',
 })
-export class LoginComponent {
-  loginForm!: FormGroup<LoginForm>;
+export class SignUpComponent {
+  signupForm!: FormGroup<SignupForm>;
 
   constructor(
     private router: Router,
     private loginService: LoginService,
     private toastService: ToastrService
   ) {
-    this.loginForm = new FormGroup({
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(6),
+      ]),
+      passwordConfirm: new FormControl('', [
         Validators.required,
         Validators.minLength(6),
       ]),
@@ -47,7 +54,7 @@ export class LoginComponent {
   // send data to DB
   submit() {
     this.loginService
-      .login(this.loginForm.value.email, this.loginForm.value.password)
+      .login(this.signupForm.value.email, this.signupForm.value.password)
       .subscribe({
         next: () => this.toastService.success('Usúario logado com sucesso!'),
         error: () => this.toastService.error('Erro ao logar'),
@@ -56,6 +63,6 @@ export class LoginComponent {
 
   // create route to signup
   navigate() {
-    this.router.navigate(['/signup']);
+    this.router.navigate(['/login']);
   }
 }
