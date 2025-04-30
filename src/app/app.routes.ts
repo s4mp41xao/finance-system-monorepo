f29e603cc448cbbcee6c941c3db2daf52aaeb539
s4mp41xao <samuelpaixao@proton.me>
2025-04-30 18:42:11 -0300

    feat: create AuthGuard to just allow logged users acess specifieds pages with canActive routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SignUpComponent } from './pages/signup/signup.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { AuthGuard } from './services/auth-guard.service';
 
 export const routes: Routes = [
   {
@@ -10,5 +12,11 @@ export const routes: Routes = [
   {
     path: 'signup',
     component: SignUpComponent,
+  },
+
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
   },
 ];
