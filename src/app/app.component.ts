444cbf1a5cf287f3fbf655db7f34e176c8681954
s4mp41xao <samuelpaixao@proton.me>
2025-05-03 20:57:27 -0300

    feat: add icon home on dashboard and dollar on expenses in sidenav with ng-icons/mynaui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { mynaDollarCircle, mynaHome } from '@ng-icons/mynaui/outline';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  viewProviders: [provideIcons({ mynaHome, mynaDollarCircle })],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgIcon } from '@ng-icons/core';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgIcon],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css',
 })
