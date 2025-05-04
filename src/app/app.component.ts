b60eda005470784bd902405955febd0f36219673
s4mp41xao <samuelpaixao@proton.me>
2025-05-03 22:30:23 -0300

    feat: ajust icons size and add button icon to open and close sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,23 @@
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { mynaDollarCircle, mynaHome } from '@ng-icons/mynaui/outline';
+import {
+  mynaDollarCircle,
+  mynaHome,
+  mynaChevronRight,
+  mynaChevronLeft,
+} from '@ng-icons/mynaui/outline';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  viewProviders: [provideIcons({ mynaHome, mynaDollarCircle })],
+  viewProviders: [
+    provideIcons({
+      mynaHome,
+      mynaDollarCircle,
+      mynaChevronRight,
+      mynaChevronLeft,
+    }),
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
