32ddc0b45627d29a1f62cdaa04bd9400f1e6d24d
s4mp41xao <samuelpaixao@proton.me>
2025-05-05 13:17:36 -0300

    feat: create CompanyRepository

--- src/main/java/com/example/spring34/__java_21/company/repository/CompanyRepository.java
+++ src/main/java/com/example/spring34/__java_21/company/repository/CompanyRepository.java
@@ -1,4 +1,10 @@
 package com.example.spring34.__java_21.company.repository;
 
-public class CompanyRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Optional<Company> findByIdAndUserId(Long companyId, Long userId);
+    void deleteByIdAndUserId(Long companyId, Long userId);
 }
