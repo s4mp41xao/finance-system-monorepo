03935c34c5ed838779ca46a5a3e2e83fb9a888e8
s4mp41xao <samuelpaixao@proton.me>
2025-05-05 23:29:49 -0300

    fix: add import to Company

--- src/main/java/com/example/spring34/__java_21/company/repository/CompanyRepository.java
+++ src/main/java/com/example/spring34/__java_21/company/repository/CompanyRepository.java
@@ -1,5 +1,6 @@
 package com.example.spring34.__java_21.company.repository;
 
+import com.example.spring34.__java_21.company.model.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
