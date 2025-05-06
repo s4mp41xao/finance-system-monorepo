f17ed831304732825f011287f807c3136470b5e5
s4mp41xao <samuelpaixao@proton.me>
2025-05-05 23:31:38 -0300

    feat: create DTO of CompanyRequest to require name and cnpj

--- src/main/java/com/example/spring34/__java_21/company/dto/request/CompanyRequest.java
+++ src/main/java/com/example/spring34/__java_21/company/dto/request/CompanyRequest.java
@@ -1,9 +1,4 @@
 package com.example.spring34.__java_21.company.dto.request;
 
-import jakarta.validation.constraints.NotBlank;
-import org.hibernate.validator.constraints.br.CNPJ;
-
-public record CompanyRequest(
-        @NotBlank String name,
-        @NotBlank @CNPJ String cnpj
-) { }
+public record CompanyRequest(String name, String cnpj){
+}
