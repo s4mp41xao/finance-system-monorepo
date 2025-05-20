214a7a8067915803b8b307852a5e40afc4e110b0
s4mp41xao <samuelpaixao@proton.me>
2025-05-19 22:56:48 -0300

    chore: remove unused line codes from company.services

--- src/app/pages/home/company/company.service.ts
+++ src/app/pages/home/company/company.service.ts
@@ -12,31 +12,11 @@ export class CompanyService {
 
   constructor(private http: HttpClient) {}
 
-  // private getAuthHeaders(): HttpHeaders {
-  //   const token = sessionStorage.getItem('auth-token');
-  //   if (!token) {
-  //     throw new Error('Token not found');
-  //   }
-
-  //   return new HttpHeaders({
-  //     'Content-Type': 'application/json',
-  //     Authorization: `Bearer ${token}`,
-  //   });
-  // }
-
   createCompany(payload: { name: string; cnpj: string }): Observable<Company> {
-    // const headers = new HttpHeaders({
-    //   'Content-Type': 'application/json',
-    // });
-
-    // const headers = this.getAuthHeaders();
-    // console.log(this.getAuthHeaders);
-
     return this.http.post<Company>(this.apiUrl, payload);
   }
 
   getCompaniesByUser(): Observable<Company[]> {
-    // const headers = this.getAuthHeaders();
     return this.http.get<Company[]>(this.apiUrl);
   }
 }
