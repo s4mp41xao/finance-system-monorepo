1774a3074b3667b32fb62e105235ce2168c3db4b
s4mp41xao <samuelpaixao@proton.me>
2025-05-24 22:35:46 -0300

    feat: creates BankAccountRepository with FindByCompanyId method, and extended Jpa methods

---
+++ src/main/java/com/example/spring34/__java_21/bankAccount/repository/BankAccountRepository.java
@@ -0,0 +1,13 @@
+package com.example.spring34.__java_21.bankAccount.repository;
+
+import com.example.spring34.__java_21.bankAccount.model.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface BankAccountRepository extends JpaRepository<BankAccount, Long> {
+
+    // search all accounts by companies
+    List<BankAccount> findByCompanyId(Long companyId);
+
+}
