da79d360f5b179e7f74f07ce1aa3a9bfb05781b5
s4mp41xao <samuelpaixao@proton.me>
2025-06-11 23:11:33 -0300

    feat(transaction-modal): add modal for creating transactions with dynamic bank account and category selection, emitting complete transaction data for API integrations

---
+++ src/app/pages/home/transactions/transaction-modal/transaction-modal.component.ts
@@ -0,0 +1,93 @@
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+} from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { TransactionCategorySelectComponent } from '../transaction-category-select/transaction-category-select.component';
+import { BankAccountService } from '../../bank-accounts/bank-accounts.service';
+
+@Component({
+  imports: [TransactionCategorySelectComponent, ReactiveFormsModule],
+  selector: 'app-transaction-modal',
+  templateUrl: './transaction-modal.component.html',
+  styleUrls: ['./transaction-modal.component.css'],
+})
+export class TransactionModalComponent implements OnInit, OnChanges {
+  @Input() companyId: number | null = null;
+  @Output() close = new EventEmitter<void>();
+  @Output() saved = new EventEmitter<any>(); // Para emitir a transação salva
+
+  form!: FormGroup;
+  bankAccounts: any[] = [];
+
+  constructor(
+    private fb: FormBuilder,
+    private bankAccountService: BankAccountService // ADICIONE ESTA LINHA
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      amount: [null, [Validators.required, Validators.min(0.01)]],
+      date: [null, Validators.required],
+      description: [''],
+      type: ['EXPENSE', Validators.required],
+      categoryId: [null],
+      bankAccountId: [null, Validators.required],
+    });
+
+    this.loadBankAccounts();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['companyId']) {
+      this.loadBankAccounts();
+    }
+  }
+
+  loadBankAccounts() {
+    if (this.companyId) {
+      this.bankAccountService.getByCompany(this.companyId).subscribe({
+        next: (accounts) => (this.bankAccounts = accounts),
+        error: (err) => (this.bankAccounts = []),
+      });
+    } else {
+      this.bankAccounts = [];
+    }
+  }
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      const formValue = this.form.value;
+
+      // categoryId agora é um objeto { id, name }
+      const category = formValue.categoryId;
+
+      const transaction = {
+        ...formValue,
+        categoryId: category?.id,
+        categoryName: category?.name,
+        bankAccountId: Number(formValue.bankAccountId),
+        creationDate: new Date().toISOString(),
+      };
+
+      this.saved.emit(transaction);
+      this.onClose();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
